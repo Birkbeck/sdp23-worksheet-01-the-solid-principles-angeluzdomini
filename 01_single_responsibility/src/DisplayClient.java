f28d2744d493ccc071d22400cb30bb9bfc87fadd
Farsana Majeed Kallungal <farsana.kallungal@transferwise.com>
2023-01-19 21:03:12 +0000

    Board and display client

---
+++ 01_single_responsibility/src/DisplayClient.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class DisplayClient implements Display {
+
+  @Override
+  public void display(List<String> spots) {
+    String formattedFirstRow = spots.get(0) + " | " + spots.get(1) + " | " + spots.get(2) + "\n"
+        + spots.get(3) + " | " + spots.get(4) + " | " + spots.get(5) + "\n"
+        + spots.get(6) + " | " + spots.get(7) + " | " + spots.get(8);
+    System.out.print(formattedFirstRow);
+  }
+}
